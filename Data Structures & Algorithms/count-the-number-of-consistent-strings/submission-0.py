0bbcb4384cf195149d8d4c6b1de48bd89a7fec89
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-09-01 15:58:50 -0500

    Add: count-the-number-of-consistent-strings - submission-0

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-0.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        '''
+        parameters: distinct char str: allowed, arr of str: words.
+        returns: NUMBER OF consistent strings in the arrau words.
+        Really asking: Hashmap Counter proble. Check if the characters in words[i] exist in count_allowed and count_allowed[i] > words[i].
+        Constraints: 
+        '''
+        count = Counter(allowed)
+        res = 0
+
+        for word in words:
+            consistent = True
+            for c in word:
+                if c not in count:
+                    consistent = False
+                    break
+            if consistent:
+                res += 1
+        return res
